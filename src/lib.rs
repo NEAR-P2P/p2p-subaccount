a112a4105ad450237395676d66ca379e2120fa99
hrpalencia <hrpmicarelli@gmail.com>
2022-06-22 14:26:55 -0400

    actualizacion

---
+++ src/lib.rs
@@ -0,0 +1,129 @@
+/*
+MIT license
+Develop by GlobalDv @2022
+*/
+
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use serde::Serialize;
+use serde::Deserialize;
+use near_sdk::{env, near_bindgen, AccountId, Promise, ext_contract, Gas, Balance};
+use near_sdk::json_types::U128;
+
+
+const GAS_FOR_TRANSFER: Gas = Gas(40_000_000_000_000);
+const CONTRACT_USDC: &str = "usdc.fakes.testnet";
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/// Objects Definition///////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#[ext_contract(ext_tranfer_usdc)]
+trait ExtTranferUsdc {
+    fn ft_transfer(&mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>
+    );
+}
+
+/*
+Near P2P Struct
+*/
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct NearP2P {
+    // Users
+    pub user_admin: AccountId,
+    pub vault: AccountId,
+}
+
+/// Initializing deafult impl
+/// We are using default inizialization for the structs
+impl Default for NearP2P {
+    fn default() -> Self {
+        Self {
+                user_admin: "contract.p2p-testnet.testnet".parse().unwrap(),
+                vault: "info.testnet".parse().unwrap(),
+            }
+    }
+}
+
+/// Implementing Struct
+#[near_bindgen]
+impl NearP2P {
+
+    pub fn transfer(&mut self, ft_token: String, receiver_id: AccountId, operation_amount: u128, fee_deducted: u128) {
+        if env::signer_account_id() == self.user_admin {
+            if ft_token == "USDC".to_string() {
+                let contract_usdc: AccountId = CONTRACT_USDC.parse().unwrap();
+                // transfer usdc to owner
+                ext_tranfer_usdc::ft_transfer(
+                    receiver_id,
+                    U128(operation_amount - fee_deducted),
+                    None,
+                    contract_usdc.clone(),
+                    1,
+                    GAS_FOR_TRANSFER,
+                );
+                if fee_deducted > 0 {
+                    // tranfer usdc fee al vault
+                    ext_tranfer_usdc::ft_transfer(
+                        self.vault.clone(),
+                        U128(fee_deducted),
+                        None,
+                        contract_usdc.clone(),
+                        1,
+                        GAS_FOR_TRANSFER,
+                    );
+                }
+            
+            } else {
+                Promise::new(receiver_id).transfer(operation_amount - fee_deducted);
+                if fee_deducted > 0 {
+                    Promise::new(self.vault.clone()).transfer(fee_deducted);
+                }
+            }    
+        } else { env::panic_str(&"Only administrators".to_string()); }
+    }
+
+    pub fn get_balance_near(self) -> Balance {
+        env::account_balance()
+    }
+}
+
+    /*#[test]
+    fn set_order() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        //let attached_deposit = env::attached_deposit();
+        let mut contract = NearP2P::default();
+        let account_id = "p2p-testnet.testnet".to_string();
+        let asset = "NEAR".to_string();
+        let price = "14.5".to_string();
+        let amount = "100".to_string();
+        let min_limit = "10".to_string();
+        let max_limit = "100".to_string();
+        let order_type = 1;
+        let payment_method = 1;
+        let orders_completed = 0;
+        let percentaje_completion = 0;
+        let badge = "super star".to_string();
+        //contract.set_offers(account_id, asset, price, amount, min_limit, max_limit, order_type, payment_method, orders_completed, percentaje_completion, badge);
+        //assert_eq!(contract.get_offers().len(), 1);
+    }
+
+    #[test]
+    fn test_lock() {
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        //let attached_deposit = env::attached_deposit();
+        let mut contract = NearP2P::default();
+        let escrow_account_id = env::predecessor_account_id();
+        let account_id = "p2p-testnet.testnet".to_string();
+        //contract.lock(account_id.to_string());
+        //print!("Locked balance: {}", contract.get_locked_balance(account_id.to_string(), escrow_account_id));
+    }*/
+
+
